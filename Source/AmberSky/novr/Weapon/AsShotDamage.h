01a3e0f10714cd331ee975e86bf3f69ac923bf91
Natalie Soltis <natalie.soltis@outlook.com>
2023-12-24 13:27:25 -0500

    Add all initial from early prototype development

---
+++ Source/AmberSky/novr/Weapon/AsShotDamage.h
@@ -0,0 +1,16 @@
+// (c) Dark Frog SGS 2023.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/DamageType.h"
+#include "AsShotDamage.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AMBERSKY_API UAsShotDamage : public UDamageType
+{
+	GENERATED_BODY()
+};
